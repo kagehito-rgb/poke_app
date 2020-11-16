6f019a616a3baf4108cd94d6b5fdb390bafb7385
kagehito-rgb <npu3tdz3mt@i.softbank.jp>
2020-11-16 12:46:34 +0900

    Initial Commit

---
+++ PokeApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PokeApp
+//
+//  Created by 中村裕紀 on 2020/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by 中村裕紀 on 2020/11/16.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
